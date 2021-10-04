c351ff5559e7fd06514d8259c3427b8eb9ead278
SarahBednarsh <sarahbednarsh@gmail.com>
2021-10-04 17:13:09 +0300

    initial code + welcome functions

---
+++ Targil0/Program9065.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome9065();
+            Welcome9845();
+            Console.ReadKey();
+        }
+        static partial void Welcome9845();
+        private static void Welcome9065()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
