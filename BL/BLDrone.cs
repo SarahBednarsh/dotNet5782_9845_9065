379d10172e41709e4caa828bddaa87753be62079
SarahBednarsh <sarahbednarsh@gmail.com>
2021-11-17 16:53:54 +0200

    jj

--- BL/BLDrone.cs
+++ BL/BLDrone.cs
@@ -8,13 +8,32 @@ namespace IBL
     {
         public partial class BL
         {
+            public Drone SearchDrone(int droneId) { }
             public void AddDrone(int id, string model, WeightCategories maxWeight, int stationIdForCharging) { }
             public void UpdateDroneName(int droneId, string newName) { }
-            public void DroneToCharge(int droneId) { }
+            public void DroneToCharge(int droneId)
+            {
+                Drone drone = SearchDrone(droneId);
+                IEnumerable < Station > stations= YieldStation();//im assuming that it returns ibl.bo stations
+
+                double minDistance = -1;
+                foreach(Station station in stations)
+                {
+                    if(station.OpenChargeSlots > 0)
+                    {
+                        minDistance = station.Location.CalcDis(drone.Location);
+                    }
+                }
+                foreach(Station station in stations)
+                {
+                    if ((station.OpenChargeSlots > 0) && (station.Location.CalcDis(drone.Location) < minDistance))
+                        minDistance = station.Location.CalcDis(drone.Location);
+                }
+                //if battery isnt enough to get there throw exception- or if t
+            }
             public void ReleaseCharging(int droneId, int timeCharging) { }
             public void PickUpAParcel(int droneId) { }
             public void DeliverAParcel(int droneId) { }
-            public Drone SearchDrone(int droneId) { }
             public IEnumerable<Drone> YieldDrone() { }
         }
     }
