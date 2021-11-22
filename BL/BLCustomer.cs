45ebd31e331d632706e03ba129d24d908d632713
SarahBednarsh <sarahbednarsh@gmail.com>
2021-11-22 19:07:05 +0200

    jj

--- BL/BLCustomer.cs
+++ BL/BLCustomer.cs
@@ -10,11 +10,18 @@ namespace IBL
         {
             public void AddCustomer(int id, string name, string phone, double longitude, double latitude)
             {
+                //if customer exists then exception-check if there is in dal
+                dalAP.AddCustomer(id, name, phone, longitude, latitude);
 
             }
             public void UpdateCustomerInfo(int customerId, string name, string phone)//not sure
             {
-
+                if (name==""&&phone=="")
+                {
+                  //  throw Exception
+                }
+                IDAL.DO.Customer customer = dalAP.SearchCustomer(customerId);
+                //deal with if it doesnt exist
             }
             public Customer SearchCustomer(int customerId)
             {
