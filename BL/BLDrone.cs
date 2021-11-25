63bf2b91775f3f8896b40c45d9533e7923ef8e04
SarahBednarsh <sarahbednarsh@gmail.com>
2021-11-25 11:40:23 +0200

    cvbnm

--- BL/BLDrone.cs
+++ BL/BLDrone.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+
 
 namespace IBL
 {
@@ -8,8 +10,20 @@ namespace IBL
     {
         public partial class BL
         {
-            public Drone SearchDrone(int droneId) { }
-            public void AddDrone(int id, string model, WeightCategories maxWeight, int stationIdForCharging) { }
+            public Drone SearchDrone(int droneId)
+            {
+                
+            }
+            private Drone CreateDrone (IDAL.DO.Drone old)
+            {
+                Drone drone = new Drone { Battery = new Random().Next(20, 40) , Id=old.Id, Location=};//need to finish
+                return drone;
+            }
+            public void AddDrone(int id, string model, WeightCategories maxWeight, int stationIdForCharging)
+            {
+                Drone drone = new Drone { Battery = new Random().Next(20, 40), Location = SearchStation(stationIdForCharging).Location, Id = id, MaxWeight = maxWeight, Model = model, Status = DroneStatuses.InMaintenance };
+                //what should we do about the parcel- maybe do automatic null?
+            }
             public void UpdateDroneName(int droneId, string newName) { }
             public void DroneToCharge(int droneId)
             {
@@ -21,14 +35,14 @@ namespace IBL
                 {
                     if(station.OpenChargeSlots > 0)
                     {
-                        minDistance = station.Location.CalcDis(drone.Location);
+                        minDistance = LocationStaticClass.CalcDis(station.Location,drone.Location);
                     }
                 }
                 foreach(Station station in stations)
                 {
-                    if ((station.OpenChargeSlots > 0) && (station.Location.CalcDis(drone.Location) < minDistance))
-                        minDistance = station.Location.CalcDis(drone.Location);
-                }
+                    if ((station.OpenChargeSlots > 0) && (LocationStaticClass.CalcDis(station.Location,drone.Location) < minDistance))
+                        minDistance = LocationStaticClass.CalcDis(station.Location,drone.Location);
+                }S
                 //if battery isnt enough to get there throw exception- or if t
                 //if it is:
                 //need to decide how to update it right- does search drone return hafnaya or whateveror is it a cpy? its a copy so what do we do
