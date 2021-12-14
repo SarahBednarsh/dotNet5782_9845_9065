286183aa50d3badf56983ee7beddc1b1f683f012
SarahBednarsh <sarahbednarsh@gmail.com>
2021-12-14 13:02:23 +0200

    fghj

---
+++ PL/PLenums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    public enum DroneStatuses { Available = 1, InMaintenance, Delivering }
+    public enum WeightCategories { Light = 1, Medium, Heavy }
+    public enum Priorities { Normal = 1, Quick, Emergency }
+    public enum States { Created = 1, Attributed, PickedUp, Delivered }
+}
