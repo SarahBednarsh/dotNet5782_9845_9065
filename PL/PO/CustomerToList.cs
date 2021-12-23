d3b2205afe785e07f5e19fadddb6c4d3652cd426
liorama <liorahmm@gmail.com>
2021-12-23 21:04:23 +0200

    חיעכ

---
+++ PL/PO/CustomerToList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace PL
+{
+    public class CustomerToList : BO.CustomerToList, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
