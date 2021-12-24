63c744aaea06ac24f3ad8f17105a4e94acb7dd1c
SarahBednarsh <sarahbednarsh@gmail.com>
2021-12-24 11:22:35 +0200

    bn

--- PL/Windows/ParcelWindows/ParcelWindow.xaml.cs
+++ PL/Windows/ParcelWindows/ParcelWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.ComponentModel;
 using System.Collections.ObjectModel;
 using BlApi;
 using System.Linq;
+using System;
 
 namespace PL
 {
@@ -21,14 +22,14 @@ namespace PL
     {
         private readonly IBL bl;
         ObservableCollection<ParcelToList> parcels;
-        private ParcelToList plParcel;
+        private Parcel plParcel;
 
         public ParcelWindow(IBL bl, ObservableCollection<ParcelToList> parcels, int parcelId)
         {
             InitializeComponent();
             this.bl = bl;
             this.parcels = parcels;
-            plParcel = parcels.Where(p => p.PTLId == parcelId).FirstOrDefault();
+            plParcel = Adapter.ParcelBotoPo(bl.SearchParcel(parcelId));
             DataContext = plParcel;
             //ActionsGrid.Visibility = Visibility.Visible;
 
@@ -44,7 +45,80 @@ namespace PL
 
             //InitializeActionsButton(plDrone);
         }
-       
+        private void InitializeActionsButton(Parcel parcel)
+        {
+            Actions.Click -= Delete_Click;
+            Actions.Click -= PickUp_Click;
+            Actions.Click -= Deliver_Click;
+            if (parcel==null)
+                throw new ArgumentNullException("No parcel");
+            if(parcel.Attribution==null)
+            {
+                Actions.Content = "Delete parcel";
+                Actions.Click += Delete_Click;
+            }            
+            else if(parcel.PickUp==null)
+            {
+                Actions.Content = "Pick up parcel";
+                Actions.Click += PickUp_Click;
+            }            
+            else if(parcel.Delivery==null)
+            {
+                Actions.Content = "Deliver parcel";
+                Actions.Click += Deliver_Click;
+            }
+        }
+
+        private void Deliver_Click(object sender, RoutedEventArgs e)
+        {
+            //bl.DeliverAParcel();
+        }
+
+        private void PickUp_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        //private void InitializeActionsButton(Drone drone)
+        //{
+        //    Actions.Click -= Charge_Click;
+        //    Actions.Click -= ReleaseCharge_Click;
+        //    Actions.Click -= Deliver_Click;
+        //    Actions.Click -= Pickup_Click;
+        //    if (drone == null)
+        //        throw new ArgumentNullException("No drone");
+        //    if (drone.Status == DroneStatuses.Available)
+        //    {
+        //        Actions.Content = "Charge";
+        //        Actions.Click += Charge_Click;
+        //        Actions2.Visibility = Visibility.Visible;
+        //        Actions2.Content = "Send to delivery";
+        //        Actions2.Click += SendToDelivery_Click;
+        //    }
+        //    else if (drone.Status == DroneStatuses.InMaintenance)
+        //    {
+        //        Actions.Content = "Release charge";
+        //        Actions.Click += ReleaseCharge_Click;
+        //        Actions2.Visibility = Visibility.Hidden;
+        //    }
+        //    else if (bl.SearchDrone(drone.Id).Parcel.PickedUpAlready)
+        //    {
+        //        Actions.Content = "Deliver parcel";
+        //        Actions.Click += Deliver_Click;
+        //        Actions2.Visibility = Visibility.Hidden;
+        //    }
+        //    else
+        //    {
+        //        Actions.Content = "Pick up parcel";
+        //        Actions.Click += Pickup_Click;
+        //        Actions2.Visibility = Visibility.Hidden;
+        //    }
+        //}
         public ParcelWindow()
         {
             InitializeComponent();
