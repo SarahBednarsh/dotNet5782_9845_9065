0b4c17e18376dd226bbb10b88b2a9eda6ab1cc1d
liorama <liorahmm@gmail.com>
2022-01-01 21:13:57 +0200

    gh

---
+++ PL/Windows/Converters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace PL
+{
+    public class BackgroundToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Brushes)
+                return (value as Brushes).Equals(Brushes.White);
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                if ((bool)value)
+                    return Brushes.White;
+            }
+            return Brushes.Red;
+        }
+    }
+}
