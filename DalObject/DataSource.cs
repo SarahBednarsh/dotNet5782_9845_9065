94e2babe6ec3e7ec7eba7900833fa8154f1afa1e
SarahBednarsh <sarahbednarsh@gmail.com>
2022-01-11 13:24:10 +0200

    fghgf

--- DalObject/DataSource.cs
+++ DalObject/DataSource.cs
@@ -37,7 +37,7 @@ namespace Dal
                 Id = r.Next(1000, 10000);
                 while (DataSource.Stations.Exists(x => x.Id == Id))
                     Id = r.Next(1000, 10000);
-                Station station = new Station() { Id = Id, Name = stationNames[i], Longitude = StaticSexagesimal.InitializeSexagesimal(r.NextDouble() + r.Next(-999, 999), "Longitude"), Latitude = StaticSexagesimal.InitializeSexagesimal(r.NextDouble() + r.Next(-999, 999), "Latitude"), ChargeSlots = r.Next(9) + 1 };
+                Station station = new Station() { Id = Id, Name = stationNames[i], Longitude = StaticSexagesimal.InitializeSexagesimal(29.489 + r.Next(0, 4), "Longitude"), Latitude = StaticSexagesimal.InitializeSexagesimal(34.361 + r.NextDouble(), "Latitude"), ChargeSlots = r.Next(9) + 1 };
                 DataSource.Stations.Add(station);//so it is a realistic number of chargeslots, and it might be full eventually
             }
             for (int i = 0; i < 10; i++)//adding drones
